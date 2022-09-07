4fd6379fd8876fbbadd054b405b0ae64bad61d4c
SnzDev <tms.working@gmail.com>
2022-09-07 11:17:27 -0300

    Added chat gateway to test socket.io

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './database/prisma.module';
 import { WhatsappModule } from './modules/whatsapp/whatsapp.module';
 import { LinesModule } from './modules/lines/lines.module';
+import { ChatGateway } from './chat.gateway';
 
 @Module({
   imports: [PrismaModule, WhatsappModule, LinesModule],
   controllers: [],
+  providers: [ChatGateway],
 })
 export class AppModule {}
