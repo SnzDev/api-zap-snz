4a2a165b6321ef1af77da0569a521c15a667e794
SnzDev <tms.working@gmail.com>
2022-09-29 15:04:02 -0300

    adcionado headless true

--- src/modules/whatsapp/whatsapp.controller.ts
+++ src/modules/whatsapp/whatsapp.controller.ts
@@ -38,7 +38,7 @@ export class WhatsappController implements OnModuleInit {
     const client = new Client({
       authStrategy: new LocalAuth({}),
       puppeteer: {
-        headless: false,
+        headless: true,
         args: ['--no-sandbox'],
       },
     });
